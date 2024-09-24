171753653cecd553e4ba5bb2b658a0c5e1d67d47
nadharm <nadharmdhiantravan@gmail.com>
2024-09-23 22:25:17 -0500

    add tpm install to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# nvim setup
 rm -rf ~/.config/nvim
 rm -rf ~/.local/share/nvim
 rm -rf ~/.local/state/nvim
-
 cp -r ./nvim ~/.config/nvim
+
+# tmux setup
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 cp ./tmux/tmux.conf ~/.tmux.conf
 cp -r ./tmux/tmux_dir ~/.tmux
